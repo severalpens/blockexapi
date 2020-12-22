06d1b29bf1bb2306bdf958c34ced8d770066c675
severalpens <paulcollinsaf@gmail.com>
2020-12-22 11:19:57 +1000

    master

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,5 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/', function(req, res, next) {
-  res.send("Welcome to blockexapi! <br> To start a feed eg blockchain.com's 'unconfirmed transactions' ws, navigate to '/v1/btc/unconfirmed/start'. To see the feed go to '/v1/btc/unconfirmed/limit/2' ");
-});
-
 
 module.exports = router;
